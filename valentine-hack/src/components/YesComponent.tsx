156c28015b640f9bc63079719cdb27a68a0eac2a
Christoforos Psychas <cpsychas@iti.gr>
2026-02-13 12:10:41 +0200

    Refactor YesComponent to include YT video and confetti

--- valentine-hack/src/components/YesComponent.tsx
+++ valentine-hack/src/components/YesComponent.tsx
@@ -1,4 +1,29 @@
+import Confetti from "react-confetti";
+import { useWindowSize } from "react-use";
 const YesComponent = () => {
-  return <img src="/giphy.gif" alt="celebration for yes answer" />;
+  const { width, height } = useWindowSize();
+
+  return (
+    <div>
+      <iframe
+        style={{ border: "none" }}
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/xb2fjZa_L74?si=BDPgUHGUWMiiD1cA&autoplay=1"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      />
+      <Confetti
+        width={width}
+        height={height}
+        numberOfPieces={350}
+        recycle={false}
+        gravity={0.04}
+      />
+      <p>Good girl.</p>
+    </div>
+  );
 };
 export default YesComponent;
